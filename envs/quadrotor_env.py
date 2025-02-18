b23986db5e77afb11e8436170055cf35f08029af
mintashkim <mkim5117@gmail.com>
2025-02-17 20:49:35 -0800

    misc

--- envs/quadrotor_env.py
+++ envs/quadrotor_env.py
@@ -504,7 +504,7 @@ class QuadrotorEnv(MujocoEnv, utils.EzPickle):
         exQ = norm(xQ - xQd)
         evQ = norm(vQ - vQd)
 
-        if xQ[3] < 0.05:
+        if xQ[2] < 0.05:
             self.num_episode += 1
             self.history_epi[self.traj_type].append(self.timestep)
             print("Env {env_num} | Ep {epi} | St {stage} | Traj: {traj_type} | Crashed | Time: {time} | Reward: {rew}".format(
